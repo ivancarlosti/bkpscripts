969eae306db0ad340b17c6fe6cdfe7dbe8142edf
Ivan Carlos <ivan@ivancarlos.com.br>
2024-10-25 23:36:39 -0300

    Update bkppostgres.sh

--- bkppostgres.sh
+++ bkppostgres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILENAMEDATE=$(date +%Y-%m-%dT%H-%M-%S) # date/time formula to compose filename
-DOCKERNAME=postgres                     # docker name to access it and send pg_all command 
+DOCKERNAME=postgres                     # docker name to access it and send pg_all command
 PGUSER=postroot                         # username with access to gather all databases using pg_dumpall, usually root
 RELATPATH=/backups                      # relative path folder where the files will be stored
 RETDAYS=3                               # number of days to retain files before deletion
